0ec5d68258130fd7a0cd7ac54a44c19bf10eeed7
ewgenij87snwork <ewgenij87snwork@gmail.com>
2024-10-07 17:05:30 +0300

    webpack.config.js: env

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 // for proper splicing of the paths
 const path = require('path');
 
-module.exports = {
-    mode: 'development', //'development'  - with comments, line breaks; 'production' -- minified
-    // "entry" -- where Webpack starts building app
-    // for proper splicing of the paths
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+module.exports = (env) => {
+    return {
+        /* mode
+        in package.json we put 'env':
+          "scripts": {
+            "build:dev": "webpack --env mode=development",
+            "build:prod": "webpack --env mode=production"
+          },
+        * */
+        mode: env.mode ?? 'development', //'development'  - with comments, line breaks; 'production' -- minified
 
-    // tells Webpack where to output the bundle it creates and how to name it.
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: '[name].[contenthash].js', //[name] -- use standard name (main), [contenthash] -- add random symbols to prevent browser cashing
-        clean: true //remove previous/old bundle
+        // "entry" -- where Webpack starts building app
+        // for proper splicing of the paths
+        entry: path.resolve(__dirname, 'src', 'index.js'),
+
+        // tells Webpack where to output the bundle it creates and how to name it.
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: '[name].[contenthash].js', //[name] -- use standard name (main), [contenthash] -- add random symbols to prevent browser cashing
+            clean: true //remove previous/old bundle
+        }
     }
 }
