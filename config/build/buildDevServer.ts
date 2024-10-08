c8e86104ed985e18d812f49bb4029794dc9801f2
ewgenij87snwork <ewgenij87snwork@gmail.com>
2024-10-08 15:35:48 +0300

    CSS Modules

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,6 @@ import { BuildOptions } from "./type/type";
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port ?? 4500,
-        open: true // for immediately open in browser (on macOs not possible set some specific browser. Will open in default)
+        // open: true // for immediately open in browser (on macOs not possible set some specific browser. Will open in default)
     }
 }
