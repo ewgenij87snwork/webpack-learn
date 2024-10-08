3b97c52e40bc6cfa9b94c5f5a164bc1902946621
ewgenij87snwork <ewgenij87snwork@gmail.com>
2024-10-08 16:18:27 +0300

    Router & FIX tsconfig.json for use import

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,10 @@ import { BuildOptions } from "./type/type";
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port ?? 4500,
+
+        // for SPA, for use just one file (index.html) instead many (about.html, shop.html)
+        // if distribute static (example nginx) then need to do proxy to index.html
+        historyApiFallback: true
         // open: true // for immediately open in browser (on macOs not possible set some specific browser. Will open in default)
     }
 }
